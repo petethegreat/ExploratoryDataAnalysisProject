edfe2759ac4e05ad9589c3df7b9ce0c4e560ebeb
Peter Thompson <peter.thompson@radimetrics.com>
2017-02-08 07:52:28 -0500

    tidied plots a little

--- plot1.R
+++ plot1.R
@@ -17,7 +17,8 @@ with(totals,
         plot(year,Emissions/1.0e6,ylab='PM 2.5 Emissions (millions of tons)',type='n')
         points(year,Emissions/1.0e6,col='red',pch=19)
         title(main="PM2.5 emissions vs year")
-        #abline(lm(Emissions/1.0e6~year))
+        abline(lm(Emissions/1.0e6~year),lty=2,col='blue')
+		legend('topright',legend=c('Total Emissions','fit line'),col=c('red','blue'),pch=c(19,NA),lty=c(NA,2))
         #title(ylab='PM 2.5 Emissions (millions of tons)')
         #title(xlab='year')
 
--- plot2.R
+++ plot2.R
@@ -1,7 +1,9 @@
 
 
 # load data
+message('loading summarySCC_PM25.rds')
 NEI <- readRDS("summarySCC_PM25.rds")
+message('loading summarySCC_PM25.rds')
 SCC <- readRDS("Source_Classification_Code.rds")
 
 # sum up all emissions by year
@@ -16,8 +18,10 @@ with(totals,
     {
         plot(year,Emissions/1.0e6,ylab='PM 2.5 Emissions (millions of tons)',type='n')
         points(year,Emissions/1.0e6,col='red',pch=19)
-        title(main="PM2.5 emissions vs year")
-        #abline(lm(Emissions/1.0e6~year))
+        title(main="Baltimore City PM2.5 emissions vs year")
+        abline(lm(Emissions/1.0e6~year),lty=2,col='blue')
+		legend('topright',legend=c('Total Emissions','fit line'),col=c('red','blue'),pch=c(19,NA),lty=c(NA,2))
+		
         #title(ylab='PM 2.5 Emissions (millions of tons)')
         #title(xlab='year')
 

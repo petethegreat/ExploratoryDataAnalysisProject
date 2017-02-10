5d6c64d640655faa1aeac242ca9572823473f359
Peter Thompson <petethegreat@gmail.com>
2017-02-09 22:56:40 -0500

    working on plt4

--- plot3.R
+++ plot3.R
@@ -7,27 +7,19 @@ message('loading summarySCC_PM25.rds')
 SCC <- readRDS("Source_Classification_Code.rds")
 NEI$type<- as.factor(NEI$type)
 
-# sum up all emissions by year
-# (total emissions of all types from all measured locations)
+# sum up all baltimore emissions by year and type
+# (total emissions from Baltimore)
 
 totals<-aggregate(Emissions ~ year+type,NEI[ NEI$fips == 24510,],sum)
 
 
-# make a plot
-# annotate with title and axis labels
+# make a plot:
+# specify x/y variables as aesthetics
 g<-ggplot(aes(year,Emissions,colour=type),data=totals)
+# add labels
+labels<-labs(x='year',y='Emissions (tons)',title='PM2.5 Emissions in Baltimore by type')
+# draw with points, add fit lines, and set y axis limits
+g + geom_point(size=3) + stat_smooth(method='lm',lty=2)  +labels + ylim(0,2500)
 
-g + geom_point()
 
-# with(totals,
-#     {
-#         plot(year,Emissions/1.0e6,ylab='PM 2.5 Emissions (millions of tons)',type='n')
-#         points(year,Emissions/1.0e6,col='red',pch=19)
-#         title(main="Baltimore City PM2.5 emissions vs year")
-#         abline(lm(Emissions/1.0e6~year),lty=2,col='blue')
-# 		legend('topright',legend=c('Total Emissions','fit line'),col=c('red','blue'),pch=c(19,NA),lty=c(NA,2))
-#
-#         #title(ylab='PM 2.5 Emissions (millions of tons)')
-#         #title(xlab='year')
-#
-#     })
+
---
+++ plot4.R
@@ -0,0 +1,44 @@
+
+library(ggplot2)
+
+# load data
+message('loading summarySCC_PM25.rds')
+NEI <- readRDS("summarySCC_PM25.rds")
+message('loading summarySCC_PM25.rds')
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI$type<- as.factor(NEI$type)
+
+# create a logical vector of the entries in SCC that are coal combustion related
+# (including lignite)
+
+coalLog<- with(SCC, 
+    grepl('[Cc]ombustion',SCC.Level.One) & 
+    (  grepl('[Cc]oal',SCC.Level.Three)  | grepl('[Cc]oal',SCC.Level.Four) |
+       grepl('[Ll]ignite',SCC.Level.Three)  | grepl('[Ll]ignite',SCC.Level.Four) 
+     ) )
+# 103 TRUE
+
+# get the codes for these
+coalcodes<-SCC$SCC[coalLog]
+
+# get the total emissions
+
+#totals<-NEI[NEI$SCC %in% coalcodes,] 
+### Point plot
+# totals<-aggregate(Emissions ~ year,NEI[ NEI$SCC %in% coalcodes,],sum)
+# g<-ggplot(aes(factor(year),Emissions),data=totals,na.rm=true)
+# g + geom_point(size=3,colour='red') 
+## add a line to this
+
+
+# boxplot
+# hard to see a trend
+coaled<-NEI[NEI$SCC %in% coalcodes,]
+
+g<-ggplot(aes(factor(year),log10(Emissions)),data=coaled,na.rm=true)
+
+g + geom_boxplot()
+
+
+
+
